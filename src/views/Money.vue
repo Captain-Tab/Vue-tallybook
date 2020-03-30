d979bb40ec692f07b706e6d27f4aee730ef686bb
Tab <d491405879@gmail.com>
2020-03-30 15:27:33 +0800

    封装数据，新建model.ts文件

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,5 @@
 <template>
   <Layout class-prefix="layout">
-    {{recordList}}
     <NumberPad :value.sync="record.amount" @submit="SaveRecord"/>
     <Types :value.sync="record.type"/>
     <Notes @update:value="onUpdateNotes"/>
@@ -15,38 +14,31 @@
   import Notes from '@/components/money/Notes.vue';
   import Tags from '@/components/money/Tags.vue';
   import {Component, Watch} from 'vue-property-decorator';
-
-
-
+  import model from '@/views/model';
 
   /*if (version < '0.0.2') {
-    const version = window.localStorage.getItem('version') || '0';
-    // 数据库版本升级，数据迁移
-    recordList.forEach(record => {
-      record.time = new Date(2020, 0, 1);
-    });
-    // 保存数据
-    window.localStorage.setItem('recordList', JSON.stringify(recordList));
-  } else if (version === '0.0.2') {
+  const version = window.localStorage.getItem('version') || '0';
+  // 数据库版本升级，数据迁移
+  recordList.forEach(record => {
+    record.time = new Date(2020, 0, 1);
+  });
+  // 保存数据
+  window.localStorage.setItem('recordList', JSON.stringify(recordList));
+} else if (version === '0.0.2') {
 
-  }
-  window.localStorage.setItem('version', '0.0.2');
+}
+window.localStorage.setItem('version', '0.0.2');
 */
 
-  type Record = {
-    tags?: string[];
-    note: string;
-    type: string;
-    amount: number;
-    time?: Date;
-  }
+  const recordList = model.fetch();
+
   @Component({
     components: {Tags, Notes, Types, NumberPad}
   })
   export default class Money extends Vue {
     tags = ['服装', '饮食', '住宿', '交通', '话费'];
-    recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
-    record: Record = {
+    recordList: RecordItem[] = recordList;
+    record: RecordItem = {
       tags: [],
       note: '',
       type: '-',
@@ -62,14 +54,14 @@
     }
 
     SaveRecord() {
-      const record2 = JSON.parse(JSON.stringify(this.record));
+      const record2 = model.clone(this.record);
       record2.time = new Date();
       this.recordList.push(record2);
     }
 
     @Watch('recordList')
     onRecordListChanged() {
-      window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
+      model.save(this.recordList);
     }
   }
 </script>
