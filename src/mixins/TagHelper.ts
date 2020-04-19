ca22d33ad3705ff028920e620234a648b967be09
Tab <d491405879@gmail.com>
2020-04-19 17:10:40 +0800

    修改打开就弹出提示的BUG

--- src/mixins/TagHelper.ts
+++ src/mixins/TagHelper.ts
@@ -1,12 +1,19 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
 
+const map: {[key: string]: string}= {
+  'tag name is duplicated': '标签名重复了'
+}
+
 @Component
 export class TagHelper extends Vue {
   createTag() {
     const inputValue = window.prompt('请输入要新增的标签名');
     if (!inputValue) {return window.alert('标签名不能为空');}
     this.$store.commit('createTag', inputValue);
+    if(this.$store.state.createTagError){
+      window.alert(map[this.$store.state.createTagError.message] || '未知错误')
+    }
   }
 }
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -9,6 +9,7 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
   state: {    // data
     createRecordError: null,
+    createTagError: null,
     recordList: [],
     tagList: [],
     currentTag: undefined
@@ -37,14 +38,15 @@ const store = new Vuex.Store({
       }
     },
     createTag(state, name: string) {
+      state.createTagError = null;
       const names = state.tagList.map(item => item.name);
       if (names.indexOf(name) >= 0) {
-        window.alert('标题名重复了');
+        state.createTagError = new Error('tag name is duplicated');
+        return;
       }
       const id = createID().toString();
       state.tagList.push({id, name: name});
       store.commit('saveTags');
-      window.alert('添加成功');
     },
     saveTags(state) {
       window.localStorage.setItem('tagList', JSON.stringify(state.tagList));
